658ca84c90d6e958d777b359240ff41e08d13783
tienpvse <good66612@gmail.com>
2025-01-24 01:21:29 +0700

    add pagination

---
+++ src/common/pagination.ts
@@ -0,0 +1,34 @@
+import { SelectQueryBuilder } from 'kysely';
+import { DB } from 'kysely-codegen';
+
+export type PaginationOptions = { page?: number; pageSize?: number };
+export type PaginationResult<T> = {
+  data: T[];
+  count: number;
+  currentPage: number;
+  totalPages: number;
+  pageSize: number;
+  hasNextPage: boolean;
+};
+
+export async function withPagination<I extends keyof DB, O>(
+  query: SelectQueryBuilder<DB, I, O>,
+  options: PaginationOptions,
+): Promise<PaginationResult<O>> {
+  const { page = 1, pageSize = 10 } = options;
+  const offset = (page - 1) * pageSize;
+  const limit = pageSize;
+  const data = await query.offset(offset).limit(limit).execute();
+  const countResult = await query
+    .select((eb) => eb.fn.countAll().as('count'))
+    .executeTakeFirst();
+  const count = Number.parseInt(countResult.count.toString());
+  return {
+    data,
+    count: data.length,
+    pageSize: limit,
+    currentPage: page,
+    totalPages: Math.ceil(count / limit),
+    hasNextPage: count > offset + limit,
+  };
+}
